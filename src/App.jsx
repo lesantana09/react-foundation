d88994f9229c55313349689c984e264c4c05fb01
Leandro Santana <leandro.santana@infosi.com.br>
2025-03-31 23:55:04 -0300

    feat:update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
+import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
 
   return (
     <div>
       <Header />
-      <Post 
-        title="Leandro Santana" 
-        content="Meu post Test" 
-      />
-      <Post 
-        title="Leonardoz Santana" 
-        content="Meu post Test" 
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post 
+            title="Leandro Santana" 
+            content="Meu post Test" 
+          />
+          <Post 
+            title="Leonardoz Santana" 
+            content="Meu post Test" 
+          />
+        </main>
+      </div>
     </div>
   )
 }
