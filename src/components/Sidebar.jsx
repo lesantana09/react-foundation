65c550c48a279716f9b4b1b3598efaf49e2708c4
Leandro Santana <leandro.santana@infosi.com.br>
2025-04-03 23:25:28 -0300

    feat:finishe sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react';
 
 export function Sidebar() {
     return (
@@ -8,11 +9,15 @@ export function Sidebar() {
                 src="https://plus.unsplash.com/premium_photo-1678565869434-c81195861939?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             />
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://github.com/lesantana09.png"/>
                 <strong>Leandro Santana</strong>
                 <span>Software Engenieer</span>
             </div>
             <footer>
-                <a href="#">Editar seu perfil</a>
+                <a href="#">
+                    <PencilLine size={20}/>
+                    Editar seu perfil
+                </a>
             </footer>
         </aside>
     );
